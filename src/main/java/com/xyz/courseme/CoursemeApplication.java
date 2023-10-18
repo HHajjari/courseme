68e59b1ce045f114da4c6c9f3c0a8cc43616958e
x034223 <hossein.hajjari@klm.com>
2023-10-18 22:52:13 +0200

    Init

---
+++ src/main/java/com/xyz/courseme/CoursemeApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.courseme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xyz/courseme/CoursemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.courseme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
